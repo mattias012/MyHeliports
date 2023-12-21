bfbaedc50bfe2e9e70b2a07bf973c5a65ad2f0c6
Mattias Axelsson <yhma23.axelma@folkuniversitetet.nu>
2023-12-21 23:47:58 +0100

    added material m3 stuff

--- app/src/main/java/com/example/myheliports/StartActivity.kt
+++ app/src/main/java/com/example/myheliports/StartActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationBarView
 
 class StartActivity : AppCompatActivity() {
 
@@ -14,12 +15,43 @@ class StartActivity : AppCompatActivity() {
 
 
         addItemButton = findViewById(R.id.addItemButton)
+        val bottomNavigation = findViewById<NavigationBarView>(R.id.bottom_navigation)
 
         addItemButton.setOnClickListener {
             val intent = Intent(this, AddLocationActivity::class.java)
             startActivity(intent)
         }
 
+        NavigationBarView.OnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.item_1 -> {
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.item_2 -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                R.id.item_3 -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                else -> false
+            }
+        }
+        bottomNavigation.setOnItemReselectedListener { item ->
+            when(item.itemId) {
+                R.id.item_1 -> {
+                    // Respond to navigation item 1 reselection
+                }
+                R.id.item_2 -> {
+                    // Respond to navigation item 2 reselection
+                }
+                R.id.item_3 -> {
+                    // Respond to navigation item 2 reselection
+                }
+            }
+        }
 
     }
 }

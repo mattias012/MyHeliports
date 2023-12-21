29e2c3d576cdc3dc33cccca181e6dd38b2f986a8
Mattias Axelsson <yhma23.axelma@folkuniversitetet.nu>
2023-12-21 08:24:21 +0100

    forgot to checkout branch

--- app/src/main/java/com/example/myheliports/MainActivity.kt
+++ app/src/main/java/com/example/myheliports/MainActivity.kt
@@ -3,14 +3,29 @@ package com.example.myheliports
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var auth : FirebaseAuth
+    lateinit var emailView : EditText
+    lateinit var passwordView : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, MapsActivity::class.java)
+        auth = Firebase.auth
 
-        startActivity(intent)
+        emailView = findViewById(R.id.emailText)
+        passwordView = findViewById(R.id.passwordText)
+
+
+
+
+//        val intent = Intent(this, MapsActivity::class.java)
+//        startActivity(intent)
     }
-}
+}   
